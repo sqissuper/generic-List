9c8fcd0ee6a8dd86eb69826a4bf6e5a81b752989
sqissuper <1764491917@qq.com>
2021-01-21 17:57:21 +0800

    泛型

---
+++ Main4.java
@@ -0,0 +1,149 @@
+
+
+import java.util.*;
+import java.util.stream.Collector;
+//class MyList<T>{
+//    public T[] val;
+//    public int size;
+//
+//    public MyList(){
+//        this.val =(T[]) new Object[10];
+//    }
+//
+//    public void setVal(T val){
+//        this.val[this.size]= val;
+//        this.size++;
+//    }
+//
+//    public T getVal(int pos){
+//        return this.val[pos];
+//    }
+
+//    @Override
+//    public String toString() {
+//        return "MyList{" +
+//                "val=" + Arrays.toString(val) +
+//                '}';
+//    }
+//}
+//class Student{
+//    public String name;
+//    public int num;
+//    public double result;
+//
+//    public Student(String name, int num, double result) {
+//        this.name = name;
+//        this.num = num;
+//        this.result = result;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Student{" +
+//                "name='" + name + '\'' +
+//                ", num=" + num +
+//                ", result=" + result +
+//                '}';
+//    }
+//}
+//class A<T> {
+//    T value;
+//
+//    A(T value) {
+//        this.value = value;
+//    }
+//
+//    T get() {
+//        return value;
+//    }
+//}
+public class Main4 {
+    public static void main(String[] args) {
+//        A<Integer> a = new A<>(1);
+//        System.out.println(a.get());
+
+    }
+
+
+//    public static void main(String[] args) {
+////        List<String> list = new ArrayList<>();
+////        list.add(0,"h");
+////        list.add(1,"e");
+////        list.add(2,"l");
+////        list.add(3,"l");
+////        list.add(4,"o");
+////        System.out.println(list);
+//        List<Integer> list = new ArrayList<>();
+//        list.add(5);
+//        list.add(2);
+//        list.add(7);
+//        list.add(4);
+//        Collections.sort(list);
+//        System.out.println(list);
+//    }
+}
+
+
+//    public static void main(String[] args) {
+//        MyList<Integer> myList = new MyList<>();
+//        MyList<String> myList1 = new MyList<>();
+//        myList1.setVal("hello");
+//        myList1.setVal("world");
+//        myList.setVal(1);
+//        myList.setVal(2);
+//        String str = myList1.getVal(0);
+//        System.out.println(str);
+//        int ret = myList.getVal(1);
+//        System.out.println(ret);
+//    }
+//    public static void main(String[] args) {
+//        MyList myList = new MyList();
+//        myList.setVal(1);
+//        myList.setVal(2);
+//        myList.setVal("shiqiang");
+//        String str = (String) myList.getVal(2);
+//        System.out.println(str);
+//        //System.out.println(myList);
+//    }
+
+
+//    public static void main(String[] args) {
+//        List<Student> list = new ArrayList<Student>();
+//        Student stu1 = new Student("张三",12,80.5);
+//        Student stu2 = new Student("李四",13,90.2);
+//        Student stu3 = new Student("李华",14,85.5);
+//        list.add(stu1);
+//        list.add(stu2);
+//        list.add(stu3);
+//
+//        System.out.println(list);
+//        String str = "hello";
+//        str = str + " world";
+//        System.out.println(str);
+//
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("hello").append(" world");
+//        System.out.println(sb);
+//
+//        StringBuilder sb1 = new StringBuilder("hello");
+//        sb1.append(" world");
+//        System.out.println(sb1);
+//
+//        StringBuilder sb3 = new StringBuilder();
+//        sb3.append("hello").append(" world");
+//        String s = sb3.toString();
+//        System.out.println(s);
+
+//        StringBuilder sb = new StringBuilder();
+//
+//        System.out.println(sb.append("helloWorld"));//拼接字符串
+//        System.out.println(sb.reverse());//反转字符串
+//        System.out.println(sb.delete(2,6));//删除指定范围的字符
+//        System.out.println(sb.insert(2,"你好").insert(3,23));//在指定位置插入可以是任意类型
+//        System.out.println(sb.replace(2, 5, "good"));//指定位置替换字符串
+
+
+
+//    }
+
+
